3c4f8598803ae5eb83e519b629c55409ca1bb1fb
Hulk Sun <sunhao.java@gmail.com>
2025-08-07 03:57:03 +0800

    chore: 修改发布方式，发布到最新的仓库

--- tools/deploy.cmd
+++ tools/deploy.cmd
@@ -19,17 +19,13 @@
 
 set profile=%1
 
-if "%profile%"=="release-lodsve" (
-  .\mvnw.cmd clean deploy -P %1 -Dmaven.test.skip=true
+if "%profile%"=="release-third" (
+  ..\mvnw.cmd clean deploy -P %1 -Dmaven.test.skip=true
 ) else (
   if "%2"=="" (
     echo The password for the gpg key to publish the maven repository cannot be empty.
     exit /b 1
   )
 
-  if not "%profile%"=="release-oss-snapshot" (
-    REM publish snapshot
-    set args=-Dgpg.passphrase=$2 -Dmaven.test.skip=false
-  )
-  .\mvnw.cmd clean deploy -P %1 %args%
+  ..\mvnw.cmd clean deploy -P %1 %args% -Dgpg.passphrase=$2 -Dmaven.test.skip=false
 )
